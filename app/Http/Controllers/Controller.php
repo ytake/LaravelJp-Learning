1b1a1f82c243806740ca1418d9e7370a1ed87568
ytake <yuuki.takezawa@comnect.jp.net>
2015-12-18 21:43:20 +0900

    init

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * Class Controller
+ */
+abstract class Controller extends BaseController
+{
+
+}
